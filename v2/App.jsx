4515ed084ca856d6db380c303954e2e0d6973047
jun233s <1227491415@qq.com>
2024-03-24 15:12:11 +0800

    使用fiber架构优化reader函数

--- v2/App.jsx
+++ v2/App.jsx
@@ -1,6 +1,5 @@
 /**@jsx JReact.createElement */
-import JReact from './core/React.js'
+import JReact from './core/React.js';
 // const App = React.createElement('div', {id: 'test'}, 'a', 'b', 'c')
-const App = <div id="test">abc</div>
-console.log(App)
-export default App
+const App = <div id="test">abc</div>;
+export default App;
--- v2/core/React.js
+++ v2/core/React.js
@@ -1,29 +1,95 @@
 const createTextNode = (val) => {
   return {
     type: 'text',
-    nodeValue: val
-  }
-} 
+    props: {
+      nodeValue: val,
+      childrenNodes: [],
+    },
+  };
+};
 const createElement = (type, props, ...children) => {
   return {
     type,
-    props,
-    childrenNodes: children.map(item => {
-      return typeof item === 'string' ? createTextNode(item) : children
-    })
+    props: {
+      ...props,
+      childrenNodes: children.map((item) => {
+        return typeof item === 'string' ? createTextNode(item) : children;
+      }),
+    },
+  };
+};
+
+let nextFiber = null;
+const reader = (el, container) => {
+  nextFiber = {
+    dom: container,
+    props: {
+      childrenNodes: [el],
+    },
+  };
+};
+
+function createDom(type) {
+  const dom =
+    type === 'text'
+      ? document.createTextNode('')
+      : document.createElement(type);
+  return dom;
+}
+function updateProps(dom, props) {
+  props &&
+    Object.keys(props).forEach((key) => {
+      if (key !== 'children') {
+        dom[key] = props[key];
+      }
+    });
+}
+function initChildren(fiber) {
+  const children = fiber.props.childrenNodes;
+  let prevChild = null;
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevChild.sibling = newFiber;
+    }
+    prevChild = newFiber;
+  });
+}
+function evenloop(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type));
+    fiber.parent.dom.append(dom);
+    updateProps(dom, fiber.props);
+  }
+  initChildren(fiber);
+  // 下一个要执行的任务
+  if (fiber.child) {
+    return fiber.child;
+  }
+  if (fiber.sibling) {
+    return fiber.sibling;
+  }
+  return fiber.parent.sibling;
+}
+function workloop(deadline) {
+  let is = false;
+  while (!is && nextFiber) {
+    nextFiber = evenloop(nextFiber);
+    is = deadline.timeRemaining() < 1;
   }
 }
-const reader = (el, container) => {
-  const dom = el.type === 'text' ? document.createTextNode(el.nodeValue) : document.createElement(el.type)
-  el.props && Object.keys(el.props).forEach(key => {
-    dom[key] = el.props[key]
-  })
-  el.childrenNodes && el.childrenNodes.forEach(el => {
-    reader(el, dom)
-  })
-  container.append(dom)
-}
+
+requestIdleCallback(workloop);
 export default {
   reader,
-  createElement
-}
+  createElement,
+};
